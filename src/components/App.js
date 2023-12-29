91beec3863a6b5a7f4281e68f193c05a3ea87862
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-12-29 16:25:20 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const change = () => {
+    setCount(count+1)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+    <p>{count}</p>
+        <button onClick={change}>click</button>
     </div>
   )
 }
